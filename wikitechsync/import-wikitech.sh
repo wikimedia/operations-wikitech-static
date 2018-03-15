df3a5ee972c144b3621af27696e4ae35afb956f0
Andrew Bogott <abogott@wikimedia.org>
2018-03-15 13:25:31 -0500

    Added import-wikitech script, which manages data and images
    Change-Id: If40a9e817ff010b3a778467048bada3f0d11ab7a

---
+++ wikitechsync/import-wikitech.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# This should be called from a cron on wikitech-static
+
+DATE=$(date '+%Y%m%d')
+
+wget https://wikitech.wikimedia.org/dumps/labswiki-${DATE}.xml.gz -O /srv/imports/labswiki-${DATE}.xml.gz -4
+cd /srv/mediawiki/w
+php maintenance/importDump.php /srv/imports/labswiki-${DATE}.xml.gz
+php maintenance/rebuildrecentchanges.php
+php maintenance/rebuildImages.php --missing
+
+rm /srv/imports/labswiki-${DATE}.xml.gz
+
+/wikitech-static/wikitechsync/get_images.py --wiki wikitech --config /wikitech-static/wikitechsync/dump_images.conf.wikitech-static
+wget https://wikitech.wikimedia.org/dumps/labswiki-${DATE}-images.tar.gz -O /srv/imports/labswiki-${DATE}-images.tar.gz -4
+cd /srv/mediawiki
+tar -xzvf /srv/imports/labswiki-${DATE}-images.tar.gz
+rm /srv/imports/labswiki-${DATE}-images.tar.gz
+
+
